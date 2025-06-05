dee41976ccf6cbf1721f2ad6cde58b375fd8ba00
joelj0el <jhoel.titirico@uab.edu.bo>
2025-06-05 15:13:36 -0400

    Test de los metodos multiplicacion se ha añadido

--- src/calculator.py
+++ src/calculator.py
@@ -4,3 +4,5 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
+def multiplicacion(a, b):
+    return a * b
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from src.calculator import suma, resta
+from src.calculator import suma, resta, multiplicacion
 
 class CalculatorTest(unittest.TestCase):
     
@@ -11,4 +11,15 @@ class CalculatorTest(unittest.TestCase):
     def test_suma(self):
         """Test the suma function."""
         self.assertEqual(suma(4, 3), 7)
+        
+    #perubea unitaria ora la funcion resta
+    def test_resta(self):
+        """Test the resta function."""
+        self.assertEqual(resta(4, 3), 1)
+        
+    #perubea unitaria ora la funcion multiplicacion
+    def test_multiplicacion(self):  
+        """Test the multiplicacion function."""
+        from src.calculator import multiplicacion
+        self.assertEqual(multiplicacion(4, 3), 12)
  
